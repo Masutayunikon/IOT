962bbfd7951053db153168a957c283e203b3e278
MasutaYunikon <quentin.robert@epitech.eu>
2024-11-16 15:38:24 +0100

    feat: ✨ pwa

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template xmlns="http://www.w3.org/1999/html">
+  <VitePwaManifest />
   <header class="bg-white">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1 max-w-12">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,10 +12,30 @@ export default defineNuxtConfig({
     "@nuxtjs/seo",
     "@nuxt/image",
     "@nuxt/icon",
-    "@nuxt/eslint"
+    "@nuxt/eslint",
+    "@vite-pwa/nuxt",
   ],
-  plugins: [
-  ],
+
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'IOT',
+      short_name: 'IOT',
+      theme_color: 'white',
+      background_color: '#ffffff',
+      icons: [
+        { src: '/arduino.svg', sizes: '192x192', type: 'image/svg' },
+      ],
+      start_url: '/',
+      display: 'standalone',
+      prefer_related_applications: true,
+    },
+    workbox: {
+      cleanupOutdatedCaches: true,
+      navigateFallback: '/en-US',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+  },
 
   // i18n module configuration
   i18n: {
